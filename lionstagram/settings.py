62f892c1cdd0a2607931059b632d9d2c1083654b
chohyoungseo <francesco.johs@gmail.com>
2021-09-05 03:41:16 +0900

    init

--- lionstagram/settings.py
+++ lionstagram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7wt2huexk@8frajnwdwxrhi4le_8h!lqkx87isyl^2firv$8u5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
